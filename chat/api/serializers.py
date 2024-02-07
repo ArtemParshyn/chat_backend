6ef9ced7938e0616c85f286f31466f0616568b33
Артем <parshyn4@gmail.com>
2024-02-07 23:20:01 +0300

    I added models for groups and migrations. Also, column group(foreignkey) was added to the message model. In consumers.py after connection to the socket, and checking unique logic, the new group will be created, using a new serializer in serializer.py.

--- chat/api/serializers.py
+++ chat/api/serializers.py
@@ -1,17 +1,32 @@
 from rest_framework import serializers
-from .models import Message
+from .models import Message, Group
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ("name", "date_create")
+
+    def create(self, validated_data):
+        name = validated_data["name"]
+        print("creating new group")
+        return Group.objects.create(
+            name=name
+        )
 
 
 class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = ('author', 'content', 'date')
+        fields = ('group', 'author', 'content', 'date')
 
     def create(self, validated_data):
+        group = validated_data["group"]
         author = validated_data["author"]
         content = validated_data["content"]
-        print("creating")
+        print("creating message")
         return Message.objects.create(
+            group=group,
             author=author,
             content=content,
         )
